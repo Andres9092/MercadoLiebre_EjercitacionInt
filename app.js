d078fb9de444aef3144e23be99f656db9d5df206
Andres9092 <richardsonandres1@gmail.com>
2022-09-13 08:40:08 -0300

    primera parte del ejercicio completa con HTML 13/9

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
 
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')))  //se puede prescidir de este termino __dirname,
+
 app.listen(3030, ()  => {
 
     console.log('Servidor 3030 corriendo')
 
 })
+
+app.get('/', (req,res) => { //
+
+    res.sendFile(path.join(__dirname,'./views/home.html'))  // mostrara por browser el archivo index.html en alojado en carpeta 'views'.
+})
+                                    // __dirname,'./views/index.html'  -> ruta ABSOLUTA.
