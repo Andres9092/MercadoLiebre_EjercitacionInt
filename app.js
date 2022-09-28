e3df1c94f9369198e97e4bb7d9cd58dc73544da0
Andres9092 <richardsonandres1@gmail.com>
2022-09-28 17:52:40 -0300

    pagina registe + login + search bar button emepzado

--- app.js
+++ app.js
@@ -16,3 +16,15 @@ app.get('/', (req,res) => { //
     res.sendFile(path.join(__dirname,'./views/home.html'))  // mostrara por browser el archivo home.html en alojado en carpeta 'views'.
 })
                                     // (__dirname,'./views/index.html')  -> ruta ABSOLUTA.
+
+app.get('/register', (req,res) => { //
+
+    res.sendFile(path.join(__dirname,'./views/register.html'))  // mostrara por browser el archivo home.html en alojado en carpeta 'views'.
+})
+                                    // (__dirname,'./views/index.html')  -> ruta ABSOLUTA.
+
+app.get('/login', (req,res) => { //
+
+    res.sendFile(path.join(__dirname,'./views/login.html'))  // mostrara por browser el archivo home.html en alojado en carpeta 'views'.
+})
+                                    // (__dirname,'./views/index.html')  -> ruta ABSOLUTA.
